003aa2f288e900b100a3200f8a54530115a37a41
potterinc <potter_h30@yahoo.com>
2019-10-28 09:47:19 +0100

    updated

--- www/js/agileController.js
+++ www/js/agileController.js
@@ -1,26 +1,41 @@
 
 $(document).ready(function () {
 
+    // Initialize the tab controller
+    tabController(0);
+
     // Agile Content Loader
     $('#agileControlViewer').load('../www/pages/products.html');
 
     // Dashboard
-    $('#wallet').click(function(){
+    $('#wallet').click(function () {
         $('#agileControlViewer').load('../www/pages/wallet.html');
     })
 
     // Bakeries
-    $('#bakery').click(function(){
+    $('#bakery').click(function () {
         $('#agileControlViewer').load('../www/pages/products.html');
     })
 
     // Checkout
-    $('#checkout').click(function(){
+    $('#checkout').click(function () {
         $('#agileControlViewer').load('../www/pages/cart.html');
     })
-    
+
     // Logout
-    $('#logout').click(function(){
+    $('#logout').click(function () {
         $('#agileControlViewer').load('../www/pages/login.html');
     })
+
+    //Tab List
+    $('#tab-list-actions').click;
+    $('#tab-list-purchase').click(function(){
+        tabController(2)
+    });
+    
+
+function tabController(tabIndex) {
+    alert(tabIndex);
+}
 })
+
